98e50f6b5212d1a66165de68613558e6f84760d3
blaineanthonywestern <blaine.a.western@gmail.com>
2020-10-11 18:50:14 +1300

    add fix to filter functionality

--- js/javascript.js
+++ js/javascript.js
@@ -1,76 +1,76 @@
-//https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_toggle_hide_show
-//https://www.w3schools.com/howto/howto_js_filter_elements.asp
-//https://codepen.io/RajRajeshDn/pen/zYGGvmJ 
-function setup() {
-	
-}
+// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_toggle_hide_show
+// https://www.w3schools.com/howto/howto_js_filter_elements.asp
+// https://codepen.io/RajRajeshDn/pen/zYGGvmJ
 
-//Main Filter Content button
-function filter_btn() {
-    //Filter content container shows as soon as window opens
-    var filter = document.getElementById("filter");
-    if (filter.style.display === "none") {
-        filter.style.display = "block";
-    } 
-    else {
-      filter.style.display = "none";
-      
-    }
+window.onload = () => {
+  filterBtn();
+  filterSelection("all");
+};
+
+function setup() {}
+
+// Main Filter Content button
+function filterBtn() {
+  // Filter content container shows as soon as window opens
+  var filter = document.getElementById("filter");
+  if (filter.style.display === "none") {
+    filter.style.display = "block";
+  } else {
+    filter.style.display = "none";
   }
-
-
-
-function filterSelection() {
-    col = false;
-    var tag = document.getElementsByClassName("button");
-    if (col) {
-        document.tag.style.backgroundColor = "#2196F3";
-    }
-    else {
-        document.tag.style.backgroundColor = "#E24E26";
-    }
 }
 
+var selectedItem = "all";
 
-//JS code that when tag is clicked, relevant content to the tag will show in the body
-filterSelection("all")
-  function filterSelection(c) {
-    var x, i;
-    x = document.getElementsByClassName("filterTag");
-    if (c == "all") c = "";
-    // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-    for (i = 0; i < x.length; i++) {
-      removeClass(x[i], "show");
-      if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
-    }
+// This function checks to see if the tab is selected and adds the class "selected"
+function highlightItem() {
+  var tag = document.getElementsByClassName("button");
+  var i;
+  for (i = 0; i < tag.length; i++) {
+    if (tag[i].id.indexOf(selectedItem) === -1) removeClass(tag[i], "selected");
+    if (tag[i].id.indexOf(selectedItem) > -1) addClass(tag[i], "selected");
+  }
 }
-  
+
+// JS code that when tag is clicked, relevant content to the tag will show in the body
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterTag");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    removeClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
+  }
+  highlightItem();
+}
+
 // Show filtered elements
 function addClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-      if (arr1.indexOf(arr2[i]) == -1) {
-        element.className += " " + arr2[i];
-      }
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
     }
+  }
 }
-  
+
 // Hide elements that are not selected
 function removeClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-      while (arr1.indexOf(arr2[i]) > -1) {
-        arr1.splice(arr1.indexOf(arr2[i]), 1); 
-      }
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
     }
-    element.className = arr1.join(" ");
+  }
+  element.className = arr1.join(" ");
 }
-  
-  /* Add active class to the current control button (highlight it)
+
+/* Add active class to the current control button (highlight it)
   var btnContainer = document.getElementById("filter");
   var btns = btnContainer.getElementsByClassName("button");
   for (var i = 0; i < btns.length; i++) {
@@ -79,6 +79,4 @@ function removeClass(element, name) {
       current[0].className = current[0].className.replace(" active", "");
       this.className += " active";
     });
-  }*/
-
-  
+  } */
